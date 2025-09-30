cf6dec0bbe6a3d18515a08205f520ec4c2cc6e5b
James Talton <6277895+jamestalton@users.noreply.github.com>
2025-09-30 15:23:04 -0400

    feat: add Scrollable component and integrate it into Integrations page for improved layout
    Signed-off-by: James Talton <6277895+jamestalton@users.noreply.github.com>

--- packages/frontend/src/routes/configuration/integrations/Integrations.tsx
+++ packages/frontend/src/routes/configuration/integrations/Integrations.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import { AppPage } from "../../../app/AppPage";
 import { AppPageHeader } from "../../../app/AppPageHeader";
 import { ChatInput } from "../../../components/chat/ChatInput";
@@ -6,9 +7,9 @@ export default function Integrations() {
   return (
     <AppPage>
       <AppPageHeader title="Integrations" />
-      <div className="flex flex-col glass rounded-3xl p-8 border grow overflow-auto max-h-full h-full">
+      <Scrollable className="glass rounded-3xl border">
         <div
-          className={`grid gap-4 grid-cols-[repeat(auto-fit,minmax(250px,1fr))]`}
+          className={`p-8 grid gap-4 grid-cols-[repeat(auto-fit,minmax(250px,1fr))] `}
         >
           {new Array(30).fill(0).map((_, i) => (
             <div key={i} className="p-8 glass rounded-2xl border">
@@ -17,8 +18,28 @@ export default function Integrations() {
             </div>
           ))}
         </div>
-      </div>
+      </Scrollable>
       <ChatInput />
     </AppPage>
+  );
+}
+
+import { ScrollArea } from "@base-ui-components/react/scroll-area";
+
+export function Scrollable(props: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <ScrollArea.Root
+      className={clsx("h-full w-full overflow-hidden", props.className)}
+    >
+      <ScrollArea.Viewport className="h-full overscroll-contain rounded-md">
+        {props.children}
+      </ScrollArea.Viewport>
+      <ScrollArea.Scrollbar className="mx-3 my-4 flex w-2 justify-center rounded opacity-0 transition-opacity delay-300 data-[hovering]:opacity-100 data-[hovering]:delay-0 data-[hovering]:duration-75 data-[scrolling]:opacity-100 data-[scrolling]:delay-0 data-[scrolling]:duration-75">
+        <ScrollArea.Thumb className="w-full rounded bg-violet-500/40" />
+      </ScrollArea.Scrollbar>
+    </ScrollArea.Root>
   );
 }
