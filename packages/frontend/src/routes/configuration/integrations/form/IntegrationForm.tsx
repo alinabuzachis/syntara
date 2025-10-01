ae2e8c7e1cb7bd59124762c92307e842934e3623
James Talton <6277895+jamestalton@users.noreply.github.com>
2025-10-01 13:20:33 -0400

    feat: update IntegrationForm layout and styling, add new buttons for integration management
    Signed-off-by: James Talton <6277895+jamestalton@users.noreply.github.com>

--- packages/frontend/src/routes/configuration/integrations/form/IntegrationForm.tsx
+++ packages/frontend/src/routes/configuration/integrations/form/IntegrationForm.tsx
@@ -12,14 +12,14 @@ export function IntegrationForm() {
     <AppPage>
       <AppPageHeader title="Configure Integration">
         <div className="grow" />
-        <button
+        {/* <button
           className="bg-white/10 px-4 py-1 rounded-full self-end"
           onClick={() => navigate(AppRoute.Configuration.Integrations.Root)}
         >
           Cancel
-        </button>
+        </button> */}
       </AppPageHeader>
-      <div className="flex gap-4 grow">
+      <div className="grid grid-cols-2 gap-6 grow">
         <Form className="flex flex-col gap-4 glass border grow p-8 rounded-3xl">
           <Select
             name="type"
@@ -37,9 +37,23 @@ export function IntegrationForm() {
             Test Integration
           </button>
         </Form>
-        <div className="flex flex-col gap-4 glass border grow p-8 rounded-3xl">
+        <div className="flex flex-col gap-4 glass border grow p-8 rounded-3xl justify-center text-balance">
           Test the integration to identify and manage the tools it provides.
         </div>
+      </div>
+      <div className="glass border px-8 py-6 rounded-3xl flex gap-4">
+        <button
+          className="bg-blue-400/40 px-4 py-1 rounded-full self-end"
+          onClick={() => navigate(AppRoute.Configuration.Integrations.Root)}
+        >
+          Add integration
+        </button>
+        <button
+          className="bg-white/10 px-4 py-1 rounded-full self-end"
+          onClick={() => navigate(AppRoute.Configuration.Integrations.Root)}
+        >
+          Cancel
+        </button>
       </div>
       <ChatInput />
     </AppPage>
