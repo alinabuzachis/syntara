1ba2c50d49b134c4be2c1b59b09a68902e7185ca
James Talton <6277895+jamestalton@users.noreply.github.com>
2025-10-16 15:53:42 -0400

    fix: restore conditional check for development environment in enableMocking function
    Signed-off-by: James Talton <6277895+jamestalton@users.noreply.github.com>

--- packages/nexus-ui/src/main.tsx
+++ packages/nexus-ui/src/main.tsx
@@ -8,9 +8,9 @@ import './index.css'
 const queryClient = new QueryClient()
 
 async function enableMocking() {
-  // if (process.env.NODE_ENV !== 'development') {
-  //   return
-  // }
+  if (process.env.NODE_ENV !== 'development') {
+    return
+  }
 
   const { worker } = await import('./mocks/browser')
 
