80e7463a76c21eb578d0e4c92ddea9bae66384c8
Kersom <9053044+nixocio@users.noreply.github.com>
2025-11-13 12:05:16 -0500

    Fix broken configuration (#39)
    Fix broken configuration

--- packages/nexus-ui/vite.config.ts
+++ packages/nexus-ui/vite.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
     strictPort: true,
     proxy: {
       '/api': {
-        target: process.env.VITE_API_URL || 'http://localhost:8000',
+        target: process.env.VITE_API_URL || 'http://localhost:3000',
         changeOrigin: true,
         secure: false,
       },
